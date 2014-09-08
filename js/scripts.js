80299384c7dcf6d3cf85eab631558374578c0230
tititos <carlosanachris@gmail.com>
2014-09-08 19:57:18 +0200

    Cambios de java

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+/* Comentario en bloque */
+
+//Comentario de linea
+
+//Prueba de funcionamiento de JS
+
+//document.write("Si funciona mi JavaScript");
+
+//Generar alerta a mi usuario
+//alert("Ventana de alerta para usuario");
+
+//Solicitar imformacion a nuestro usuario
+/*
+var nombre = prompt("Por favor introduzca su nombre");
+document.write("<h1>Bienvenid@: " + nombre + "</h1><br />");
+
+var edad = prompt("Por favor introduzca su edad");
+document.write("Su edad es: " + edad + " años");*/
+
+// Operaciones matematicas
+/*
+var numUno = 5;
+var numDos = 2;
+var numTres = 2;
+
+var suma = numUno + numDos;
+
+var resultado = suma / numTres;
+
+console.log(suma);
+console.log(resultado);
+*/
+
+// Operaciones matematicas Personalizadas
+var numUno = parseInt(prompt("Introduzca Primer valor"));
+var numDos = parseInt(prompt("Introduzca Segundo valor"));
+var numTres = parseInt(prompt("Introduzca Tercer valor"));
+
+var suma = numUno + numDos;
+
+var resultado = suma / numTres;
+console.log("La operacion es la siguiente: " + numUno + " + " + numDos + " / " + numTres);
+document.write("Su resultado es :" + resultado);
+console.log(suma);
+console.log(resultado);
